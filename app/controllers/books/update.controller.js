65e5cea65e585691c15281859173eaa5f7621172
Corvax <vlvete1990@yandex.ru>
2023-12-12 00:06:10 +0300

    chore: update

--- app/controllers/books/update.controller.js
+++ app/controllers/books/update.controller.js
@@ -1,3 +1,41 @@
+import Book from "#app/models/book.js";
+
 export default async function bookUpdate(request, response){
-  response.end('bookUpdate');
+  const fromId = parseInt(request.params.id ?? '0');
+
+  const book = await Book.findOne({
+    where: {
+      id: fromId
+    }
+  });
+
+  const {
+    title,
+    description,
+    genre,
+    fullName,
+    image,
+    year,
+    numberPages,
+    publishing,
+    notes,
+    read,
+    buy
+  } = request.body;
+
+  await book.update({
+    title,
+    description,
+    genre,
+    fullName,
+    image,
+    year,
+    numberPages,
+    publishing,
+    notes,
+    read,
+    buy
+  });
+
+  response.end(JSON.stringify(book));
 }
