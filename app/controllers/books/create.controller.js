dd85517364955f7fd78c9182528c0a6b5a41755f
Corvax <vlvete1990@yandex.ru>
2023-12-11 23:31:51 +0300

    chore: add create book controller

--- app/controllers/books/create.controller.js
+++ app/controllers/books/create.controller.js
@@ -1,9 +1,32 @@
 import Book from "#app/models/book.js";
 
 export default async function bookCreate(request, response){
-  const { title } = request.body;
-  console.log(request.body)
-  // const books = await Book.create({ title });
-  // response.end(JSON.stringify(books));
-  response.end('bookCreate')
+  const {
+    title,
+    description,
+    genre,
+    fullName,
+    image,
+    year,
+    numberPages,
+    publishing,
+    notes,
+    read,
+    buy
+  } = request.body;
+
+  const books = await Book.create({
+    title,
+    description,
+    genre,
+    fullName,
+    image,
+    year,
+    numberPages,
+    publishing,
+    notes,
+    read,
+    buy
+  });
+  response.end(JSON.stringify(books));
 }
