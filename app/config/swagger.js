77120714839db17ca24358174343efd31e527252
Corvax <vlvete1990@yandex.ru>
2023-12-11 10:46:35 +0300

    chore: add swagger config

---
+++ app/config/swagger.js
@@ -0,0 +1,18 @@
+import { HOST } from './app.js'
+
+export default {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My Book App',
+      version: '0.0.1',
+      description: "REST API for my Book App"
+    },
+		servers: [
+			{
+				url: HOST,
+			},
+		],
+  },
+  apis: ['./app/routes/*.js'],
+};
--- app/globals/swagger.js
+++ app/globals/swagger.js
@@ -1,22 +1,6 @@
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'My Book App',
-      version: '0.0.1',
-      description: "REST API for my Book App"
-    },
-		servers: [
-			{
-				url: "http://localhost:5000/",
-			},
-		],
-  },
-  apis: ['./app/routes/*.js'],
-};
+import options from '../config/swagger.js';
 
 const swaggerSpec = swaggerJsdoc(options);
 console.log(swaggerSpec)
