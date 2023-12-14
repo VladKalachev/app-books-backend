1e2b5ae550493f84c2ec70cdd724a181e4a1e349
Corvax <vlvete1990@yandex.ru>
2023-12-14 23:54:15 +0300

    chore: add token model

--- src/app/models/token.model.ts
+++ src/app/models/token.model.ts
@@ -1,20 +1,32 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import sequelize from '../globals/sequelize';
 
-const TokenModel = sequelize.define('Token', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  refreshToken: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+class Token extends Model {
+  declare id: number;
+  declare user: string;
+  declare refreshToken: string;
+}
 
-export default TokenModel;
+Token.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    refreshToken: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'token',
+  },
+);
+
+export default Token;
