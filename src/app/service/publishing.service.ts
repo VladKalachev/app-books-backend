39441f63a55897f2329ec29c24bdb54a49fcb60e
Corvax <vlvete1990@yandex.ru>
2024-03-14 22:52:04 +0300

    chore: add create publishing service

--- src/app/service/publishing.service.ts
+++ src/app/service/publishing.service.ts
@@ -4,6 +4,10 @@ class PublishingService {
   async all() {
     return await PublishingModel.findAll();
   }
+
+  async create(title: string) {
+    return await PublishingModel.create({ title });
+  }
 }
 
 export default new PublishingService();
