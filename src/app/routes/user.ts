f0c128c1bda9dbe299eb293f72bb787be3a44ec5
Corvax <vlvete1990@yandex.ru>
2024-03-19 23:19:02 +0300

    feature: add swagger user

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -48,16 +48,82 @@ export default function routesUser(app: Application) {
   );
 
   /**
-   * @openapi
-   * /api/user/logout
-   *  post
+   * @swagger
+   * /api/user/logout:
+   *   post:
+   *     summary: Разлогин пользователя
+   *     description: Разлогин пользователя
+   *     tags:
+   *      - User
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Server Error
    */
   app.post('/api/user/logout', userController.logout);
 
+  /**
+   * @swagger
+   * /api/activate/:link:
+   *   get:
+   *     summary: Получение ссылки активации пользователя
+   *     description: Получение ссылки активации пользователя
+   *     tags:
+   *      - User
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Server Error
+   */
   app.get('/api/activate/:link', userController.activate);
+
+  /**
+   * @swagger
+   * /api/user/refresh:
+   *   get:
+   *     summary: Рефреш токена
+   *     description: Рефреш токена
+   *     tags:
+   *      - User
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Server Error
+   */
   app.get('/api/user/refresh', userController.refresh);
 
+  /**
+   * @swagger
+   * /api/users:
+   *   get:
+   *     summary: Получение списка пользователей
+   *     description: Получение списка пользователей
+   *     tags:
+   *      - User
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Server Error
+   */
   app.get('/api/users', auth, userController.getUsers);
 
+  /**
+   * @swagger
+   * /api/usersWithBooks:
+   *   get:
+   *     summary: Получение списка пользователей с книгами
+   *     description: Получение списка пользователей с книгами
+   *     tags:
+   *      - User
+   *     responses:
+   *       200:
+   *         description: Success
+   *       500:
+   *         description: Server Error
+   */
   app.get('/api/usersWithBooks', auth, userController.getUsersWithBooks);
 }
