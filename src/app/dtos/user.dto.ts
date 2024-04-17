fc94d26c05316283f38386056f4e2aa5fd9b877a
Corvax <vlvete1990@yandex.ru>
2024-04-17 19:57:08 +0300

    feature: add field for admin

--- src/app/dtos/user.dto.ts
+++ src/app/dtos/user.dto.ts
@@ -4,10 +4,12 @@ export default class User {
   email: string;
   id: number;
   isActivated: boolean;
+  isAdmin: boolean;
 
   constructor(model: UserModel) {
     this.email = model.email;
     this.id = model.id;
     this.isActivated = model.isActivated;
+    this.isAdmin = model.isAdmin;
   }
 }
